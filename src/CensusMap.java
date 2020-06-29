30375b98f6d965d31f39fa13a83f4f1fb244639d
utt-zachery <55007266+utt-zachery@users.noreply.github.com>
2020-06-29 14:54:53 -0400

    Removed empty nodes

--- src/CensusMap.java
+++ src/CensusMap.java
@@ -1,12 +1,9 @@
-import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Stack;
-
 import javax.imageio.ImageIO;
 
 public class CensusMap {
@@ -44,28 +41,4 @@ public class CensusMap {
 		censusData.put(addressHash, toAdd);
 	}
 	
-	public void insertSpacers() {
-		int lastAddress = 0;
-		
-		Stack<Integer> toPut = new Stack<Integer>();
-		Stack<Node> toPutNode = new Stack<Node>();
-		
-		for (Map.Entry<Integer,Node> entry : censusData.entrySet()) {
-			if (entry.getKey() - lastAddress > 1) {
-				for (int i= lastAddress+1; i < entry.getKey(); i++) {
-					toPut.add(i);
-					toPutNode.add(new EmptyNode(i % width,i / width));
-				}
-			}
-			lastAddress = entry.getKey();
-		}
-		
-		while (toPut.empty() == false) {
-			censusData.put(toPut.pop(),toPutNode.pop());
-		}
-		
-		for (int i= lastAddress+1; i < width*height; i++) {
-			censusData.put(i, new EmptyNode(i % width,i / width));
-		}
-	}
 }
--- src/startup.java
+++ src/startup.java
@@ -15,12 +15,15 @@ public class startup {
 		CensusMap baseMap = new CensusMap(100,100);
 		for (int x=0; x < 100; x++)
 			for (int y=0; y < 100; y++) {
-				if (x%3==0)
-				baseMap.addVoter(x, y, democrat);
-				else if (x%3==1)
+				
+				int random_int = (int)(Math.random() * (2 - 1 + 1) + 2);
+				
+				if (random_int==2)
+					baseMap.addVoter(x, y, democrat);
+				else if (random_int==3)
 					baseMap.addVoter(x, y, republican);
 			}
-		baseMap.insertSpacers();
+		
 		baseMap.saveImage("output.png", 10);
 	}
 
